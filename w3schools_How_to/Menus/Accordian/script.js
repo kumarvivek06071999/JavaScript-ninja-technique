5c0b2bca152aba9c1de2ee1169e66801119f2c34
Vivek kumar <kumarvivek06071999@gmail.com>
2024-01-23 13:21:45 +0530

    Accordian by w3schools

---
+++ w3schools_How_to/Menus/Accordian/script.js
@@ -0,0 +1,19 @@
+const acc = document.getElementsByClassName("accordion")
+console.log(acc)
+
+let i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active")
+        let panel = this.nextElementSibling
+
+
+        if (panel.style.display === "block") {
+            panel.style.display = "none"
+        } else {
+            panel.style.display = "block"
+        }
+
+
+    })
+}
